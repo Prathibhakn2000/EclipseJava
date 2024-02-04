9f398e4b6a354126fac8a6a3b5693fa4baf13a54
Prathibhakn2000 <prathibhakn.xworkzgmail.com>
2024-02-04 22:06:04 +0900

    access specifiers

--- Objects/src/com/xworkz/vegetables/like/Bag.java
+++ Objects/src/com/xworkz/vegetables/like/Bag.java
@@ -5,7 +5,7 @@ public class Bag {
 	String material;
 	String color;
 	
-	private Bag()  //private const -> it access only same class not another class
+	private Bag()  //private const -> it access only same class  not another class from same package
 	{
 		System.out.println("running Bag no param const");
 	}
--- Objects/src/com/xworkz/vegetables/like/Bird.java
+++ Objects/src/com/xworkz/vegetables/like/Bird.java
@@ -5,7 +5,7 @@ public class Bird {
 	String type;
 	String color;
 	
-	protected Bird()
+	protected Bird()  //protected constructor -> it access both new and super()for same package classes but other package class only super()
 	{
 		System.out.println("running no param Bird const");
 	}
