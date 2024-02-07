4d3fb555496e80e1a11fd4beeb049f1e1ae4c0d6
Prathibhakn2000 <prathibhakn.xworkzgmail.com>
2024-02-07 12:29:06 +0900

    Casting

--- MedicalShop/src/com/xworkz/handwash/HandWashRunner.java
+++ MedicalShop/src/com/xworkz/handwash/HandWashRunner.java
@@ -13,22 +13,40 @@ public class HandWashRunner {
 		Road road = new Road();
 		HandWash handwash = new HandWash();
 		road.runHandWash(handwash);
+		System.out.println(handwash.aroma);
+		handwash.clean();
+		
 		
 		System.out.println("___________________________");
 		
 		
 		Sanitizer handwash1 = new Sanitizer();
 		road.runHandWash(handwash1);
+		System.out.println(handwash1.quality);
+		handwash1.disinfect();
+		System.out.println(handwash1.aroma);
+		handwash1.clean();
 		
 		System.out.println("___________________________");
 		
 		Soap handwash2 = new Soap();
 		road.runHandWash(handwash2);
+		System.out.println(handwash2.price);
+		handwash2.foam();
+		System.out.println(handwash2.aroma);
+		handwash2.clean();
+		
 		
 		System.out.println("___________________________");
 		
 		Lux handwash3 = new Lux();
 		road.runHandWash(handwash3);
+		System.out.println(handwash3.manifactringYear);
+		handwash3.makeYoung();
+		System.out.println(handwash3.aroma);
+		handwash3.clean();
+		System.out.println(handwash3.price);
+		handwash3.foam();
 		
 		
 		
